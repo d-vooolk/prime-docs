7b493d06679a4c0a27b8c22ce593a44a7bffeb9d
volk <nestwedman@gmail.com>
2025-10-28 18:26:45 +0300

    change litter list to random code

--- src/utils/randomIndexForDocs.js
+++ src/utils/randomIndexForDocs.js
@@ -1,5 +1,5 @@
 export function generateRandomCode() {
-    const russianLetters = 'АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЫЭЮЯ';
+    const russianLetters = 'АБВГДЕЖЗИКЛМНОПРСТУФХЧШЯ';
     const randomDigit = Math.floor(Math.random() * 10).toString();
     const randomLetter = russianLetters[Math.floor(Math.random() * russianLetters.length)];
     return randomDigit + randomLetter;
